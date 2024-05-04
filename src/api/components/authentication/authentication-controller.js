85e34694f2931a2ce1f09dc1fbce4b34f797cc0b
FelixPrajna <felix.535230121@stu.untar.ac.id>
2024-05-04 13:40:31 +0700

    nomor 2 saya membuat login attemps maximal 5 kali jika sudah 5 kali dan salah semua akunnya akan ke lock dalam 30 menit setelah 30 menit maka akun dapat terbuka lagi, jika salah 3 kali dan percobaan ke 4 benar maka hitungan login attempt failed akan reset menjadi 0

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,8 +1,14 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
+// untuk menghitung email apa saja yang gagal login
+const failedLoginAttempts = {};
+
+const LOGIN_ATTEMPT_LIMIT = 5;
+const LOGIN_LOCK_TIME = 30 * 60 * 1000; // 30 minutes in milliseconds
+
 /**
- * Handle login request
+ * menghandle login request
  * @param {object} request - Express request object
  * @param {object} response - Express response object
  * @param {object} next - Express route middlewares
@@ -12,6 +18,25 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
+    // mengecek apakah email tersebut sudah mencapai limit percobaan
+    if (
+      failedLoginAttempts[email] &&
+      failedLoginAttempts[email].attemptCount >= LOGIN_ATTEMPT_LIMIT
+    ) {
+      const lastAttemptTimestamp = failedLoginAttempts[email].timestamp;
+      const currentTime = new Date().getTime();
+      // mengecek apakah waktunya akun kita ke locked sudah selesai apa belum
+      if (currentTime - lastAttemptTimestamp < LOGIN_LOCK_TIME) {
+        throw errorResponder(
+          errorTypes.INVALID_CREDENTIALS,
+          'Too many failed login attempts. Account locked.'
+        );
+      } else {
+        // Reset login attempts
+        delete failedLoginAttempts[email];
+      }
+    }
+
     // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
@@ -19,11 +44,25 @@ async function login(request, response, next) {
     );
 
     if (!loginSuccess) {
+      // Record failed login attempt
+      if (!failedLoginAttempts[email]) {
+        failedLoginAttempts[email] = {
+          attemptCount: 1,
+          timestamp: new Date().getTime(),
+        };
+      } else {
+        failedLoginAttempts[email].attemptCount++;
+        failedLoginAttempts[email].timestamp = new Date().getTime();
+      }
+
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
         'Wrong email or password'
       );
     }
+
+    // Reset login attempts apabila login berhasil
+    delete failedLoginAttempts[email];
 
     return response.status(200).json(loginSuccess);
   } catch (error) {
